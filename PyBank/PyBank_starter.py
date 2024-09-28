83c40fda5844d78babaf5898459a433da530cf1b
ButcherAdam <77205692+ButcherAdam@users.noreply.github.com>
2024-09-27 20:39:07 -0500

    Add files via upload

---
+++ PyBank/PyBank_starter.py
@@ -0,0 +1,81 @@
+# -*- coding: UTF-8 -*-
+"""PyBank Homework Starter File."""
+
+# Dependencies
+import csv
+import os
+
+# Files to load and output (update with correct file paths)
+file_to_load = os.path.join("Resources", "budget_data.csv")  # Input file path
+file_to_output = os.path.join("analysis", "budget_analysis.txt")  # Output file path
+
+# Define variables to track the financial data
+total_months = 0
+total_net = 0
+# Add more variables to track other necessary financial data
+net_change_months_list = []
+net_change_list = []
+
+# Open and read the csv
+with open(file_to_load) as financial_data:
+    reader = csv.reader(financial_data)
+
+    # Skip the header row
+    header = next(reader)
+
+    # Extract first row to avoid appending to net_change_list
+    first_row = next(reader)
+    temp_profit = int(first_row[1])
+
+    # Track the max and min profit months and values
+    max_profit_month = first_row[0]
+    max_profit = int(first_row[1])
+    min_profit_month = first_row[0]
+    min_profit = int(first_row[1])
+
+    # Track the total and net change
+    total_net += temp_profit
+    total_months += 1
+
+    # Process each row of data
+    for row in reader:
+        # Track the total
+        total_net += int(row[1])
+        total_months += 1
+
+        # Track the net change
+        net_change_list.append(int(row[1]) - int(temp_profit))
+        net_change_months_list.append(row[0])
+        temp_profit = int(row[1])
+        temp_net_change = net_change_list[-1]
+
+        # Calculate the greatest increase in profits (month and amount)
+        if max_profit < temp_net_change:
+            max_profit_month = net_change_months_list[-1]
+            max_profit = temp_net_change
+
+        # Calculate the greatest decrease in losses (month and amount)
+        if min_profit > temp_net_change:
+            min_profit_month = net_change_months_list[-1]
+            min_profit = temp_net_change
+
+
+# Calculate the average net change across the months
+average_net_change = round(sum(net_change_list) / len(net_change_list),2)
+
+# Generate the output summary
+
+output = f'''Financial Analysis
+----------------------------
+Total Months: {total_months}
+Total: ${total_net}
+Average Change: ${average_net_change}
+Greatest Increase in Profits: {max_profit_month} (${max_profit})
+Greatest Decrease in Profits: {min_profit_month} (${min_profit})'''
+
+# Print the output
+print(output)
+
+# Write the results to a text file
+with open(file_to_output, "w") as txt_file:
+    txt_file.write(output)
